b9ab013a91f5da2fa8203be21e56f16b62aa325d
Arthur Marques Diniz <arthurmdiniz@outlook.com.br>
2025-10-07 20:31:55 -0300

    Add files via upload

--- Revisao_E02_Sonic.java
+++ Revisao_E02_Sonic.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Revisao_E02_Sonic {
     public static void main(String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
-        int novosAliados, esperança = 0, medo = 0;
-        int desafio1, desafio2, desafio3, confiança = 0, desanimo = 0;
+        int novosAliados, esperanca = 0, medo = 0;
+        int desafio1, desafio2, desafio3, confianca = 0, desanimo = 0;
         double media;
         int estrategiasAnalisadas, determinacao = 0, incerteza = 0;
         int positivos, negativos;
@@ -12,11 +12,9 @@ public class Revisao_E02_Sonic {
         System.out.print("Quantos novos aliados Sonic, Knuckles e Tails conseguiram encontrar? ");
         novosAliados = scanner.nextInt();
         if (novosAliados > 0) {
-            esperança = novosAliados * 15;
-            System.out.println("A equipe ganhou " + esperança + " pontos de Esperança.");
+            esperanca = novosAliados * 15;
         } else {
             medo = 40;
-            System.out.println("A equipe ganhou " + medo + " pontos de Medo.");
         }
 
         System.out.print("Qual a pontuação obtida no Desafio 1 (0-10)? ");
@@ -28,7 +26,7 @@ public class Revisao_E02_Sonic {
 
         media = (desafio1 + desafio2 + desafio3) / 3;
         if (media >= 7) {
-            confiança = 60;
+            confianca = 60;
         } else {
             desanimo = 60;
         }
@@ -44,15 +42,15 @@ public class Revisao_E02_Sonic {
         } 
 
         System.out.println("\nResumo dos Pontos da Equipe:"
-        +"\nPontos de Esperança: " + esperança
+        +"\nPontos de esperanca: " + esperanca
         + "\nPontos de Medo: " + medo
-        + "\nPontos de Confiança: " + confiança
+        + "\nPontos de confianca: " + confianca
         + "\nPontos de Desânimo: " + desanimo
         + "\nPontos de Determinação: " + determinacao
         + "\nPontos de Incerteza: " + incerteza
         );
 
-        positivos = esperança + confiança + determinacao;
+        positivos = esperanca + confianca + determinacao;
         negativos = medo + desanimo + incerteza;
 
         if (positivos > negativos) {
